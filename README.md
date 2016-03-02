2cbfb703f5c75a51b1c1dcc85035ce37cebb1d50
jongrover <hello@jonathangrover.com>
2016-03-02 12:48:19 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Responsive Flip Carousel
+
+Work In Progress...
+
+Allows switching between multiple albums. This was deisgned for a student project example.
---
+++ js/responsive-flip-carousel.js
@@ -0,0 +1,48 @@
+$(function () {
+
+  $('nav a').click(function (event) {
+    event.preventDefault();
+    var target = $(this).attr('href');
+    $('.portfolio').removeClass('active');
+    $(target).addClass('active');
+  });
+
+  var portfolios = [];
+
+  $('.portfolio').each(function (index, ele) {
+   $(ele).data('eq', index);
+   var portfolio = { 
+     id: $(this).attr('id'),  
+     activeSlide: 0, 
+     numSlides: $(this).find('.slide').size()
+   };
+   portfolios.push(portfolio);
+  });
+
+  $('.portfolio a.next').click(function (event) {
+    event.preventDefault();
+    var eq = $(this).parent().data('eq');
+    var portfolio = portfolios[eq];
+    if (portfolio.activeSlide < portfolio.numSlides - 1) {
+      portfolio.activeSlide += 1;
+    } else {
+      portfolio.activeSlide = 0;
+    }
+    $('#'+portfolio.id).find('.slide').removeClass('active');
+    $('#'+portfolio.id).find('.slide').eq(portfolio.activeSlide).addClass('active');
+  });
+
+  $('.portfolio a.prev').click(function (event) {
+    event.preventDefault();
+    var eq = $(this).parent().data('eq');
+    var portfolio = portfolios[eq];
+    if (portfolio.activeSlide > 0) {
+      portfolio.activeSlide -= 1;
+    } else {
+      portfolio.activeSlide = portfolio.numSlides - 1;
+    }
+    $('#'+portfolio.id).find('.slide').removeClass('active');
+    $('#'+portfolio.id).find('.slide').eq(portfolio.activeSlide).addClass('active');
+  });
+
+});
